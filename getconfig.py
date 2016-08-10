798522ace9bffc4da1ffab490a8c77d0e5f1ed67
zhanghuichao <zhanghuichao@bytedance.com>
2016-08-10 10:37:21 +0800

    compress print
    Change-Id: Ie92ff009c65bb37f0f001cf11a5693c50d24633f

--- getconfig.py
+++ getconfig.py
@@ -5,10 +5,4 @@ def getconfig():
     body = util.request("http://10.6.131.78:6001")
     conf = json.loads(body)
     print conf
-    ip = conf["ip"] 
-    port = conf["port"] 
-    clients = conf["clients"]
-    interval = conf["interval"]
-    at_sec = conf["at_sec"]
-
-    return ip, port, clients, interval, at_sec
+    return conf["ip"], conf["port"], conf["clients"], conf["interval"], conf["at_sec"]
